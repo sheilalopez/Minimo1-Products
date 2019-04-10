6b50f8a1751a9eefc17888851c4b05f222e62890
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-04-10 21:24:08 +0200

    added public, MyProduct

--- src/main/java/upc/edu/dsa/MyProduct.java
+++ src/main/java/upc/edu/dsa/MyProduct.java
@@ -1,4 +1,19 @@
 package upc.edu.dsa;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
 public interface MyProduct {
+    public List<Producto> ProductosOrdenadosPorPrecio (int idProducto) throws ProductoNotFoundException;
+    public List<Pedido> PedidosOrdenados (int idPedido) throws PedidoNotFoundException;
+    public List<Producto> ProductosOrdenadosPorVenta (int idProducto ) throws ProductoNotFoundException;
+    Pedido serveAnOrder();
+    public void HacerUnPedido (String idUser, Pedido pedido) throws UserNotFoundException;
+
+
+    void addUser(String user);
+    void addProducto(Producto p);
+    HashMap<String, Usuario> allUsers();
+    int size();
+    public
 }
