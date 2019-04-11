a3daebd7fa661385fe27e063e7787f4a4d5408b8
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-04-11 11:04:58 +0200

    changes in MyProductImplementation

--- src/main/java/upc/edu/dsa/MyProduct.java
+++ src/main/java/upc/edu/dsa/MyProduct.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public interface MyProduct {
     public List<Producto> ProductosOrdenadosPorPrecio () throws ProductoNotFoundException;
-    public List<Pedido> PedidosOrdenados (int idPedido) throws PedidoNotFoundException;
-    public List<Producto> ProductosOrdenadosPorVenta (int idProducto ) throws ProductoNotFoundException;
+    public List<Pedido> PedidosOrdenados (String User) throws UserNotFoundException;
+    public List<Producto> ProductosOrdenadosPorVenta ()throws ProductoNotFoundException;
     Pedido serveAnOrder();
     public void HacerUnPedido (String idUser, Pedido pedido) throws UserNotFoundException;
 
--- src/main/java/upc/edu/dsa/MyProductImplementation.java
+++ src/main/java/upc/edu/dsa/MyProductImplementation.java
@@ -35,42 +35,102 @@ public class MyProductImplementation implements MyProduct{
     }
 
     @Override
-    public List<Pedido> PedidosOrdenados(int idPedido) throws PedidoNotFoundException {
-        return null;
-    }
+    public List<Pedido> PedidosOrdenados(String user) throws UserNotFoundException {
+        List<Pedido> pedidos = new LinkedList<>();
+        User usuario = this.users.get(user);
+        if (usuario != null){
+            log.info("El usuario es: " + usuario);
+            pedidos = usuario.getPedidos();
+        } else {
+            throw new UserNotFoundException();
+        }
+        log.info("Historial de pedidos: " + pedidos);
+        return pedidos;
 
-    @Override
-    public List<Producto> ProductosOrdenadosPorVenta(int idProducto) throws ProductoNotFoundException {
-        return null;
-    }
 
-    @Override
-    public Pedido serveAnOrder() {
-        return null;
-    }
-
-    @Override
-    public void HacerUnPedido(String idUser, Pedido pedido) throws UserNotFoundException {
 
     }
 
     @Override
-    public void addUser(String user) {
+    public List<Producto> ProductosOrdenadosPorVenta() throws ProductoNotFoundException {
+        List<Producto> productos = new LinkedList<>();
+        productos.addAll(this.productos);
+        Collections.sort(productos, new Comparator<Producto>() {
+            @Override
+            public int compare(Producto o1, Producto o2) {
+                return (-1)*(o1.getVentas() - o2.getVentas());
+            }
+        });
+        log.info("Lista de productos ordenados por ventas" + productos);
+        return productos;
+    }
+
+    @Override
+   public Pedido serveAnOrder() {
+        //si servimos un pedido, tenemos que eliminarlo de la lista de pedidos "pendientes"
+        Pedido p = this.pedidos.pop(); //que hace exactamente el pop? es para listas solamente?
+        log.info("Order served: " +p); //supongo que coge el último pedido de la lista
+        procces(p);
+
+
+
+    }
+
+    private void procces(Pedido p) {
+    }
+
+    private Producto getProducto(String producto) {
+        log.info("I'm in");
+        Producto p = null;
+        for(int i = 0; i< this.productos.size(); i++) {
+            if(producto.equals(this.productos.get(i).getNombreProducto())){
+                p = this.productos.get(i);
+            }
+        }
+        return p;
+    }
+
+    @Override
+    public void HacerUnPedido(String idUser, Pedido p) throws UserNotFoundException {
+        User usuario = this.users.get(idUser);
+        log.info("USUARIO OBTENIDO: " + usuario);
+
+        if(usuario != null){
+            p.setUser(usuario);
+            this.pedidos.add(p);
+            log.info("Lista de pedidos: " + pedidos);
+
+        }else {
+            throw new UserNotFoundException();
+        }
+
+
+
+    }
+
+    @Override
+    public void addUser(int idUser, String user ) {
+        users.put(user,new User(idUser,user));
 
     }
 
     @Override
     public void addProducto(Producto p) {
+        this.productos.add(p);
 
     }
 
     @Override
     public HashMap<String, User> allUsers() {
-        return null;
+        HashMap<String, User> ret = new HashMap<>();
+        ret.putAll(this.users);
+
+        return ret;
     }
 
     @Override
     public int size() {
-        return 0;
+        log.info("Tamaño: " + this.productos.size());
+        return this.productos.size();
     }
 }
